dc72f4f9638e03683062c03477856358377bf7ee
Pochepayka <vovapo2004@gmail.com>
2025-10-10 03:00:39 +0300

    feat: add feature-squash (squashed commit)

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("hello word!")
 print("person2 change file")
+print("person1 change file for merge squash")
--- utils.py
+++ utils.py
@@ -1,2 +1,2 @@
 print("hello until.py")
-
+print("person1 change for commit squash")
